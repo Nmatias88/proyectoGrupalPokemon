4cf47ae138c80c77a78bb8dfc9f8194e82456509
Matias Nasrala <mnasrala@bqbsa.com>
2024-05-24 19:20:32 -0300

    ultimos modificaciones

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ window.addEventListener('scroll', function () {
         navbar.classList.remove('scrolled');
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+    const boxContainer = document.querySelector('.box');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        boxContainer.style.display = navMenu.classList.contains('active') ? 'none' : 'flex';
+    });
+});
